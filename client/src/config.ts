f261b3337434332d959b2360f5e67e9766ba6c4c
Aron Schüler <aron.schueler@ambient.digital>
2025-04-08 11:06:18 +0200

    fix: correct port of backend

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,7 +6,7 @@ export const BACKEND = {
   // Base HTTP URL
   BASE_URL: isProduction ? 'https://backend.final-circle.com' : 'http://localhost:5173',
   // WebSocket URL
-  WS_URL: isProduction ? 'wss://backend.final-circle.com:8000/ws' : 'ws://localhost:8000/ws',
+  WS_URL: isProduction ? 'wss://backend.final-circle.com:8000/ws' : 'ws://localhost:8080/ws',
 };
 
 export default BACKEND; 
